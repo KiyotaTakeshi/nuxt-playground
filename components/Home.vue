8d1cd75f989e65335f3d74b5dfa22eeea111a861
Takeshi Kiyota <t.kiyota@gxp.co.jp>
2021-10-18 15:12:28 +0900

    mock から値を取得し store の中身を表示できることを確認

---
+++ components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="employee in employees" :key="employee.id">
+      {{ employee }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  created() {
+    this.$store.dispatch('employee/fetchEmployees')
+  },
+  // eslint-disable-next-line vue/order-in-components
+  computed: {
+    ...mapGetters({
+      employees: 'employee/employees',
+    }),
+  },
+}
+</script>
