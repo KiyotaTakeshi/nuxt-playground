938426a08285a9325d63586475ada48989fc1e9f
Takeshi Kiyota <t.kiyota@gxp.co.jp>
2021-10-18 14:49:38 +0900

    init commit

---
+++ store/employee.js
@@ -0,0 +1,55 @@
+export const state = () => ({
+  employees: [
+    {
+      id: 0,
+      name: 'sample',
+      department: 'president',
+    },
+  ],
+})
+
+export const mutations = {
+  setEmployees(state, payload) {
+    state.employees = payload
+  },
+  setId(state, payload) {
+    state.employees[payload.index].id = payload.id
+  },
+  setName(state, payload) {
+    state.employees[payload.index].name = payload.name
+  },
+  setDepartment(state, payload) {
+    state.employees[payload.index].department = payload.department
+  },
+}
+
+export const actions = {
+  async registerEmployee({ commit }, payload) {
+    await commit('setEmployees', payload)
+  },
+  async fetchEmployees({ commit }) {
+    const res = await this.$axios.get('/employees')
+    await commit('setEmployees', res.data)
+  },
+  async fetchEmployee({ commit }, id) {
+    const res = await this.$axios.get(`/employees/${id}`)
+    await commit('setId', {
+      index: res.data.id,
+      id: res.data.id
+    })
+    await commit('setName', {
+      index: res.data.id,
+      name: res.data.name
+    })
+    await commit('setDepartment', {
+      index: res.data.id,
+      department: res.data.department
+    })
+  },
+}
+
+export const getters = {
+  employees(state) {
+    return state.employees
+  }
+}
